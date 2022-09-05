8b288dc4fe7afec28b46760c1abfb64cda9092c2
kanthakran-six <kanthakarn@six.network>
2022-09-05 16:50:34 +0700

    update feature token monitor

--- src/page/Home/Home.tsx
+++ src/page/Home/Home.tsx
@@ -3,8 +3,9 @@ import { Button, Container, FormControl, Navbar } from "react-bootstrap";
 import Box from '@mui/material/Box';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
-
-
+import { ITokenMonitor, KEY_TOKEN_MONITOR } from "../../types/ITokenMonitor";
+import Web3 from "web3";
+import { AbiItem } from "web3-utils";
 
 const itemData = [
   {
@@ -44,23 +45,96 @@ interface IOpenseaTop10Collection {
   address: string
   image_url: string
 }
+interface ITokenPrice {
+  name: string
+  price: string
+}
 // const sdk = require('api')('@opensea/v1.0#mxj1ql5k6c0il');
 // import sdk from "@opensea/opensea";
 // sdk.retrievingCollections({offset: '0', limit: '10'})
 //   .then(res => console.log(res))
 //   .catch(err => console.error(err));
 const Home = () => {
+  const [tokensMonitor, setTokensMonitor] = React.useState<ITokenPrice[]>([]);
   useEffect(() => {
-    // const mainLoad = async () => {
-    //   const options = { method: 'GET', headers: { Accept: 'application/json' } };
+    const mainLoad = async () => {
 
-    //   const res = await fetch('https://api.opensea.io/api/v1/collections?offset=0&limit=10', options)
-    //   alert(res.json())
-    //   // .then(response => response.json())
-    //   // .then(response => console.log(response))
-    //   // .catch(err => console.error(err));
-    // }
-    // mainLoad()
+      let stored = localStorage.getItem(KEY_TOKEN_MONITOR);
+      let tokenPricce: ITokenPrice[] = [];
+      if (stored) {
+        const tokens: ITokenMonitor[] = JSON.parse(stored);
+        tokens.map(async (token) => {
+          const web3 = new Web3("https://bsc-dataseed1.binance.org/")
+          const getPairAbi: AbiItem[] = [{
+            "constant": true,
+            "inputs": [
+              {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+              },
+              {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+              }
+            ],
+            "name": "getPair",
+            "outputs": [
+              {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+              }
+            ],
+            "payable": false,
+            "stateMutability": "view",
+            "type": "function"
+          }];
+          const pancakeRouter = "0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73"
+          const contractFactoryPancake = new web3.eth.Contract(getPairAbi, pancakeRouter)
+          const lpAddr = await contractFactoryPancake.methods.getPair(token.tokenAddress, token.stableCoinAddress).call()
+          const getReservesAbi: AbiItem[] = [{
+            "constant": true,
+            "inputs": [],
+            "name": "getReserves",
+            "outputs": [
+              {
+                "internalType": "uint112",
+                "name": "_reserve0",
+                "type": "uint112"
+              },
+              {
+                "internalType": "uint112",
+                "name": "_reserve1",
+                "type": "uint112"
+              },
+              {
+                "internalType": "uint32",
+                "name": "_blockTimestampLast",
+                "type": "uint32"
+              }
+            ],
+            "payable": false,
+            "stateMutability": "view",
+            "type": "function"
+          }]
+          const pairContract = new web3.eth.Contract(getReservesAbi, lpAddr)
+          const resPair = await pairContract.methods.getReserves().call()
+          const price = resPair._reserve1 / resPair._reserve0
+          tokenPricce.push({
+            name: token.name,
+            price: (+resPair._reserve1 / +resPair._reserve0).toFixed(3),
+          })
+
+        })
+        setTokensMonitor(tokenPricce);
+      }
+      // .then(response => response.json())
+      // .then(response => console.log(response))
+      // .catch(err => console.error(err));
+    }
+    mainLoad()
 
   }, []);
   return (<>
@@ -70,11 +144,11 @@ const Home = () => {
 
     {/* <Container className="w-100 h-100 p-3"> */}
     <h1>⭐️ FEED ⭐️</h1>
-      <h2>OPENSEA TOP RANK</h2>
-      <Box sx={{ width: 350, height: 450, overflowY: 'scroll' }}>
-        <ImageList variant="masonry" cols={4} gap={2}>
-          {itemData.map((item) => (
-            <a target='_blank' href={item.link}>
+    <h2>OPENSEA TOP RANK</h2>
+    <Box sx={{ width: 350, height: 450, overflowY: 'scroll' }}>
+      <ImageList variant="masonry" cols={4} gap={2}>
+        {itemData.map((item) => (
+          <a target='_blank' href={item.link}>
             <ImageListItem key={item.img}>
               <img
                 src={`${item.img}?w=248&fit=crop&auto=format`}
@@ -83,10 +157,17 @@ const Home = () => {
                 loading="lazy"
               />
             </ImageListItem>
-            </a>
-          ))}
-        </ImageList>
-      </Box>
+          </a>
+        ))}
+      </ImageList>
+    </Box>
+    <br />
+    {tokensMonitor.map((token, index) => {
+      return <div key={index}>
+        <h2>{token.name} : {token.price}
+        </h2>
+      </div>
+    })}
     {/* </Container> */}
   </>)
 };
---
+++ src/types/ITokenMonitor.ts
@@ -0,0 +1,7 @@
+export interface ITokenMonitor {
+    tokenAddress: string;
+    chain: string;
+    stableCoinAddress: string;
+    name: string;
+}
+export const KEY_TOKEN_MONITOR = "KEY_TOKEN_MONITOR";
